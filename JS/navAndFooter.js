5542de9479e5280ce3ca2ec10501b0cafbed0789
maramdab <146175281+maramdab@users.noreply.github.com>
2023-11-15 05:36:50 +0300

    navAndFooter.css was updated
    the nav bar has been made responsive

--- JS/navAndFooter.js
+++ JS/navAndFooter.js
@@ -1,2 +1,8 @@
 
-  
+function openNav() {
+    document.getElementById("mySidenav").style.width = "220px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
