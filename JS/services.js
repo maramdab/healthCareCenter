bb12958d2221644eef4c1cea00188095f1539bc3
maramdab <146175281+maramdab@users.noreply.github.com>
2023-11-24 07:29:56 +0300

    responsive for services and about.html was created

--- JS/services.js
+++ JS/services.js
@@ -67,9 +67,14 @@ updateNavigationButtons();
                     spaceBetween: 20,
                 },
                 768: {
-                    slidesPerView: 4,
-                    spaceBetween: 40,
+                    slidesPerView: 2,
+                    spaceBetween: 30,
                 },
+                792:{
+                    slidesPerView: 3,
+                    spaceBetween: 10,
+                },
+              
                 1024: {
                     slidesPerView: 3,
                     spaceBetween: 10,
